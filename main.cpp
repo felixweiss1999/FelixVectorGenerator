178d171b9b04783be38ff507c8e74e963c9959bc
felixweiss1999 <felixweiss1999@gmail.com>
2023-09-29 12:14:21 +0200

    static lib created

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "FelixVectorGenerator.hpp"
-#include <iostream>
-
-int main(){
-    std::vector<int> vec = FelixVectorGenerator::getUniformVec(100, 2, 8);
-    for(auto num : vec) std::cout << num << '\n';
-    return 0;
-}
